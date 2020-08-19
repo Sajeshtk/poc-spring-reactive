7ac2ef48af3c7315a281230c9728e5abd4b3b1ed
Sajesh T Kayyath <sajesh.kayyath@sourcebits.com>
2020-08-19 08:19:01 +0530

    Adding kafka to the mix

--- src/main/java/hello/GreetingHandler.java
+++ src/main/java/hello/GreetingHandler.java
@@ -1,6 +1,8 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -8,10 +10,18 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
+
 @Component
 public class GreetingHandler {
 
+    @Autowired
+    private KafkaTemplate<Object, Object> template;
+
     public Mono<ServerResponse> hello(ServerRequest request) {
+
+            this.template.send("topic1", "Hello at : "+ LocalDateTime.now());
+
         return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
                 .body(BodyInserters.fromValue("Hello, Spring!"));
     }
